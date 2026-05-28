40afc0afc046ff0bb871896568fc21c83946067c
Marko Stokic <m.markostokic@gmail.com>
2026-05-28 19:03:55 +0200

    Fixed Guardsman invalidation of Proxy pattern

--- src/dk/via/pro2/assignment_3/Guardsman.java
+++ src/dk/via/pro2/assignment_3/Guardsman.java
@@ -2,6 +2,8 @@ package dk.via.pro2.assignment_3;
 
 public class Guardsman implements TreasureRoomDoor {
   private TreasureRoom treasureRoom;
+  private TreasureRoomReadProxy currentReadProxy;
+  private TreasureRoomWriteProxy currentWriteProxy;
   private int readers = 0;
   private boolean writing = false;
 
@@ -16,11 +18,16 @@ public class Guardsman implements TreasureRoomDoor {
       wait();
     }
     readers++;
+    currentReadProxy = new TreasureRoomReadProxy(treasureRoom);
     return new TreasureRoomReadProxy(treasureRoom);
   }
 
   @Override
   public synchronized void releaseRead() {
+    if (currentReadProxy != null) {
+      currentReadProxy.release();
+      currentReadProxy = null;
+    }
     readers--;
     notifyAll();
   }
@@ -32,11 +39,16 @@ public class Guardsman implements TreasureRoomDoor {
       wait();
     }
     writing = true;
+    currentWriteProxy = new TreasureRoomWriteProxy(treasureRoom);
     return new TreasureRoomWriteProxy(treasureRoom);
   }
 
   @Override
   public synchronized void releaseWrite() {
+    if (currentWriteProxy != null) {
+      currentWriteProxy.release();
+      currentWriteProxy = null;
+    }
     writing = false;
     notifyAll();
   }
